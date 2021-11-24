86e831c92668087708b880a2e6109abdbbf1e14f
shital bramhe <shitalbramhe@gmail.com>
2021-11-24 18:16:21 +0530

    [shital] ADD: hash table

---
+++ Hash_Table/Hash_Table/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Hash_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hashtables using LinkedList");
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "Be");
+            hash.Add("2", "Or");
+            hash.Add("3", "Not");
+            hash.Add("4", "To");
+            hash.Add("5", "Be");
+
+            Console.WriteLine(hash.GetV("5"));
+            Console.WriteLine(hash.GetV("0"));
+        }
+    }
+}
